698c02bdcd748898618e96a5c7b1464c52aa6fc3
José Lourival Souto Júnior <jsouto@inone.com.br>
2018-04-10 14:12:21 -0300

    integração com o redux

--- ex-componente-controlado/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { combineReducers, createStore } from 'redux'
-import Field from "./field";
-
-ReactDOM.render(
-    <Field initialValue='Teste' />,
-    document.getElementById('app')
-)
